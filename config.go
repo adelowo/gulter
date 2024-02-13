760f438cb25424e4f4462eefa5001a4242a540a4
Lanre Adelowo <yo@lanre.wtf>
2024-02-14 00:32:31 +0100

    set up basic middleware

---
+++ config.go
@@ -0,0 +1,3 @@
+package gulter
+
+type Option func()
---
+++ handler.go
@@ -0,0 +1,11 @@
+package gulter
+
+import "net/http"
+
+func Gulter() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		})
+	}
+}
